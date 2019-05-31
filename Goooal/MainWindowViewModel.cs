5ddb3948b22e5d6164d6e1a42c3716f1d059d49f
Андрей Сошин <a_soshin@mail.ru>
2019-05-31 16:48:52 +0300

    44

---
+++ Goooal/MainWindowViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Goooal
+{
+    public class MainWindowViewModel: NotifyPropertyChanged
+    {
+        private string m_Team1 = "Команда1";
+        public string Team1
+        {
+            get => m_Team1;
+            set
+            {
+                m_Team1 = value;
+                OnPropertyChanged("Team1");
+            }
+        }
+        private string m_Team2 = "Команда2";
+        public string Team2
+        {
+            get => m_Team2;
+            set
+            {
+                m_Team2 = value;
+                OnPropertyChanged("Team2");
+            }
+        }
+        private int m_Fouls1 = 0;
+        public int Fouls1
+        {
+            get => m_Fouls1;
+            set
+            {
+                m_Fouls1 = value;
+                OnPropertyChanged("Fouls1");
+            }
+        }
+        private int m_Fouls2 = 0;
+        public int Fouls2
+        {
+            get => m_Fouls2;
+            set
+            {
+                m_Fouls2 = value;
+                OnPropertyChanged("Fouls2");
+            }
+        }
+        private string m_PlayName = "Название игры";
+        public string PlayName
+        {
+            get => m_PlayName;
+            set
+            {
+                m_PlayName = value;
+                OnPropertyChanged("PlayName");
+            }
+        }
+        private int m_Score1 = 0;
+        public int Score1
+        {
+            get => m_Score1;
+            set
+            {
+                m_Score1 = value;
+                OnPropertyChanged("Score1");
+            }
+        }
+        private int m_Score2 = 0;
+        public int Score2
+        {
+            get => m_Score2;
+            set
+            {
+                m_Score2 = value;
+                OnPropertyChanged("Score2");
+            }
+        }
+    }
+}
